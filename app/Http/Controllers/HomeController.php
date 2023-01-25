a7a416dd11a832901cce3c151da75cbc1220cb84
FawwazKats <fawwazkautsar1907912@upi.edu>
2023-01-25 09:20:04 +0700

    Mengambil data dari DB (migrations)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class HomeController extends Controller
 {
     public function index(){
         $name = 'Kats';
-        $tasks = ['Kuliah', 'Praktik Coding', 'Tidur'];
+        // $tasks = ['Kuliah', 'Praktik Coding', 'Tidur'];
+        $tasks = DB::table('tasks')->get();
 
         return view('welcome', [
             'name' => $name,
